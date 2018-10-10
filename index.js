56bd7883c8868b8a499ae91c2040c8531cc4b36a
derrick b <dbozich1@gmail.com>
2018-10-10 03:31:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  return document.querySelector("input[name='name']").value.trim();
+}
+
+function addNewElementAsLi(){
+  let list = document.querySelector('.employee-list');
+  let li = document.createElement("li");
+  li.innerText = retrieveEmployeeInformation();
+  list.appendChild(li);
+}
+
+function addNewLiOnClick(){
+  let button = document.querySelector("input[type='submit']");
+  button.addEventListener("click", () => {
+    addNewElementAsLi();
+    document.querySelector("input[name='name']").value = ''
+  });
+}
+
+function clearEmployeeListOnLinkClick(){
+  let anchor = document.querySelector('a');
+  anchor.addEventListener("click", () => {
+
+    let list = document.querySelector('.employee-list').children;
+
+  }
+
+}
