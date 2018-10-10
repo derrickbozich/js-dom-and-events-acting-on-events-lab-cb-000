558a9757426872d2e04962d1d4563719ce4b1c8d
derrick b <dbozich1@gmail.com>
2018-10-10 03:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,10 +27,6 @@ function addNewLiOnClick(){
 
 function clearEmployeeListOnLinkClick(){
   let anchor = document.querySelector('a');
-  anchor.addEventListener("click", () => {
 
-    // let list = document.querySelector('.employee-list');
-
-  }
 
 }
